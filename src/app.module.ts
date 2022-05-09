4d185676f5c5e6ba4cd59842ebb77b146e5b0c4e
hchowdary <hchowdary@qualminds.com>
2022-05-09 20:19:46 +0530

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { Module } from '@nestjs/common';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { EmailController } from './email.controller';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.sendgrid.net',
+        auth: {
+          user: 'apikey',
+          pass: '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',
+        },
+      },
+      template: {
+        dir: join(__dirname, 'mails'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+  controllers: [AppController, EmailController],
+  providers: [AppService],
+})
+export class AppModule {}
